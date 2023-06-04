5e67cb3e52fb1c184fc8b78f18e2e4a55fc49cbf
Bruce D'Arcus <bdarcus@gmail.com>
2023-06-04 06:25:42 -0400

    chore(proc): add bench
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

---
+++ processor/benches/proc_bench.rs
@@ -0,0 +1,20 @@
+use bibliography::{InputBibliography as Bibliography};
+use criterion::{criterion_group, criterion_main, Criterion};
+use csln_processor::Processor;
+use csln_processor::{load_bibliography_from_file, load_style_from_file};
+use style::Style;
+
+fn sort_benchmark(c: &mut Criterion) {
+    let style: Style = load_style_from_file("examples/style.csl.yaml");
+    let bibliography: Bibliography = load_bibliography_from_file("examples/ex1.bib.yaml");
+    let processor: Processor = Processor::new(style, bibliography, "en-US".to_string());
+    c.bench_function("sorting references", |b| {
+        b.iter(|| {
+            let refs = processor.get_references();
+            processor.sort_references(refs);
+        })
+    });
+}
+
+criterion_group!(benches, sort_benchmark);
+criterion_main!(benches);
