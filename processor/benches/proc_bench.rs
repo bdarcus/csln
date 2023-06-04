f56a6c91445667951102377e10f0be534ace1ffc
Bruce D'Arcus <bdarcus@gmail.com>
2023-06-04 07:59:06 -0400

    chore(proc): add rendering bench
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- processor/benches/proc_bench.rs
+++ processor/benches/proc_bench.rs
@@ -4,7 +4,7 @@ use csln_processor::Processor;
 use csln_processor::{load_bibliography_from_file, load_style_from_file};
 use style::Style;
 
-fn sort_benchmark(c: &mut Criterion) {
+fn proc_benchmark(c: &mut Criterion) {
     let style: Style = load_style_from_file("examples/style.csl.yaml");
     let bibliography: Bibliography = load_bibliography_from_file("examples/ex1.bib.yaml");
     let processor: Processor = Processor::new(style, bibliography, "en-US".to_string());
@@ -14,7 +14,12 @@ fn sort_benchmark(c: &mut Criterion) {
             processor.sort_references(refs);
         })
     });
+    c.bench_function("rendering references", |b| {
+        b.iter(|| {
+            processor.render_references();
+        })
+    });
 }
 
-criterion_group!(benches, sort_benchmark);
+criterion_group!(benches, proc_benchmark);
 criterion_main!(benches);
