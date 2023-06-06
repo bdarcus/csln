ef89f79b70ff5485ac4c2f94ab6c81cb25aea691
Bruce D'Arcus <bdarcus@gmail.com>
2023-06-06 15:52:07 -0400

    feat(citation): add the model
    Adjust the converted typescript model file to be more idiomatic.
    
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- cli/src/makeschemas.rs
+++ cli/src/makeschemas.rs
@@ -4,22 +4,27 @@ use std::fs::File;
 use std::io::Write;
 
 use style::Style;
-
+use citation::Citation;
 use bibliography::InputBibliography;
 
 fn main() {
     fs::create_dir_all("schemas").unwrap();
 
     let style_schema = schema_for!(Style);
+    let citation_schema = schema_for!(Citation);
     let bib_schema = schema_for!(InputBibliography);
 
     let style_json_output = serde_json::to_string_pretty(&style_schema).unwrap();
+    let citation_json_output = serde_json::to_string_pretty(&citation_schema).unwrap();
     let bib_json_output = serde_json::to_string_pretty(&bib_schema).unwrap();
 
+    let mut citation_file = File::create("schemas/citation.json").unwrap();
     let mut style_file = File::create("schemas/style.json").unwrap();
     let mut bib_file = File::create("schemas/bibliography.json").unwrap();
     style_file.write_all(style_json_output.as_bytes()).unwrap();
+    citation_file.write_all(citation_json_output.as_bytes()).unwrap();
     bib_file.write_all(bib_json_output.as_bytes()).unwrap();
-    println!("Wrote style schema to schemas/style.json");
     println!("Wrote bibliography schema to schemas/bibliography.json");
+    println!("Wrote citation schema to schemas/citation.json");
+    println!("Wrote style schema to schemas/style.json");
 }
