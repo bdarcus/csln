778fe2583cfb33c02784eec1b3c53785718b2c3a
Bruce D'Arcus <bdarcus@gmail.com>
2023-06-16 16:02:35 -0400

    feat(cli): clapify
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,25 +2,29 @@ use bibliography::InputBibliography as Bibliography;
 use bibliography::HasFile;
 use citation::Citation;
 use processor::Processor;
-use std::env;
 use style::Style;
+use clap::Parser;
+
+#[derive(Parser,Default,Debug)] 
+#[clap(author="Bruce D'Arcus", version, about="A CLI for CSLN")]
+pub struct Opts {
+    #[clap(short, long)]
+    /// The path to the CSLN style file
+    style: String,
+    #[clap(short, long)]
+    /// The path to the CSLN bibliography file
+    bibliography: String,
+    #[clap(short, long)]
+    /// The path to the CSLN locale file
+    locale: String,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 4 {
-        panic!("Please provide the paths of the style, locale, bibliography, and citation files as command line arguments.");
-    }
-
-    let style_path: &String = &args[1];
-    let style: Style = Style::from_file(style_path);
-
-    let bibliography_path: &String = &args[2];
-    let bibliography: Bibliography = Bibliography::from_file(bibliography_path);
+    let opts = Opts::parse();
+    let style: Style = Style::from_file(&opts.style);
+    let bibliography: Bibliography = Bibliography::from_file(&opts.bibliography);
     let citations: Vec<Citation> = Vec::new();
-
-    let locale_path: &String = &args[3];
-    let locale = style::locale::Locale::from_file(locale_path);
-
+    let locale = style::locale::Locale::from_file(&opts.locale);
     let processor: Processor = Processor::new(style, bibliography, citations, locale);
     let rendered_refs = processor.render_references();
     println!("{}", serde_json::to_string_pretty(&rendered_refs).unwrap());
