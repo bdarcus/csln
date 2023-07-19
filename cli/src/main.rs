5a6c11479e60b9d467aad791c20770593fa42d01
Bruce D'Arcus <bdarcus@gmail.com>
2023-07-19 07:13:55 -0400

    feat(proc): add refs_to_string placeholder
    I can't figure out how I want to do #105, so just added a basic
    refs_to_string function, so I have some final rendering.
    
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -3,7 +3,7 @@ use csln::bibliography::HasFile;
 use csln::bibliography::InputBibliography as Bibliography;
 use csln::citation::Citation;
 use csln::style::Style;
-use processor::Processor;
+use processor::{refs_to_string, Processor};
 
 #[derive(Parser, Default, Debug)]
 #[clap(author = "Bruce D'Arcus", version, about = "A CLI for CSLN")]
@@ -27,5 +27,6 @@ fn main() {
     let locale = csln::style::locale::Locale::from_file(&opts.locale);
     let processor: Processor = Processor::new(style, bibliography, citations, locale);
     let rendered_refs = processor.render_references();
-    println!("{}", serde_json::to_string_pretty(&rendered_refs).unwrap());
+    println!("{}", refs_to_string(rendered_refs));
+    //println!("{}", serde_json::to_string_pretty(&rendered_refs).unwrap());
 }
